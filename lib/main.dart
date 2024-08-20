ded73fc15c37b6419e125da4a2b21e15de6f2acd
Ayush Kumar <github@ayukmr.com>
2024-08-19 22:42:40 -0700

    Format main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:fleuron/widget/home.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() => runApp(
   const ProviderScope(child: Home()),
